57005505a40789febfecce2a4d94e8562a48e1b4
ytinrete <cloudli@163.com>
2016-09-21 19:46:43 +0800

    basic function
    this can be considered the first version

--- GoMessageBoard.go
+++ GoMessageBoard.go
@@ -4,9 +4,12 @@ import(
 	"encoding/json"
 	"bufio"
     "fmt"
-    "log"
     "os"
     "errors"
+    "net/http"
+    "sync"
+    "io/ioutil" 
+    // "strings"
 )
 
 type Thread struct{
@@ -14,32 +17,51 @@ type Thread struct{
 	Content string
 }
 
-var list []Thread
+type List struct{
+	List []Thread
+	mux sync.Mutex
+}
+
+func (l* List)add(t Thread) error {
+	l.mux.Lock()
+	l.List = append(l.List, t)
+	err := saveToFile(t)
+	defer l.mux.Unlock()
+	return err
+}
+
+func (l* List)get() []Thread {
+	l.mux.Lock()
+	defer l.mux.Unlock()
+	return l.List
+}
+
+var list List
 
 func loadFromFile() error {
 	file, err := os.Open("data.txt")
     if err != nil {
-        log.Fatal(err)
         return err
     }
     defer file.Close()
     scanner := bufio.NewScanner(file)
-    list = []Thread{}
+    list = List{
+    	List:[]Thread{},
+    }
     for scanner.Scan() {
     	line := scanner.Text()
     	fmt.Println("reading text:", line)
 
     	newobj := Thread{}
     	if err := json.Unmarshal([]byte(line), &newobj); err !=nil{
-    			fmt.Println("error Unmarshal:", line)
-    		}else{
-    			fmt.Println("success Unmarshal:", line)
-    			list = append(list, newobj)
-    		}
+			fmt.Println("error Unmarshal:", line)
+		}else{
+			fmt.Println("success Unmarshal:", line)
+			list.List = append(list.List, newobj)
+		}
     }
 
     if err := scanner.Err(); err != nil {
-        log.Fatal(err)
         return err
     }
     return nil
@@ -56,7 +78,6 @@ func saveToFile(t Thread) error {
 	}
 	var line []byte
 	if res, err := json.Marshal(t); err !=nil{
-		log.Fatal(err)
         return err
 	}else{
 		line = res
@@ -64,7 +85,7 @@ func saveToFile(t Thread) error {
 
 	file, err := os.OpenFile("data.txt", os.O_APPEND|os.O_WRONLY, 0600)
 	if err != nil {
-	    log.Fatal(err)
+	    
         return err
 	}
 	defer file.Close()
@@ -74,15 +95,114 @@ func saveToFile(t Thread) error {
 	
 }
 
+func getListJsonStr()(string, error) {
+	if res, err := json.Marshal(list.get()); err !=nil{
+        return "", errors.New("json parse error")
+	}else{
+		fmt.Println("json parse success:", string(res))
+		return string(res), nil		
+	}
+}
+
+func handleGetList(w http.ResponseWriter, r * http.Request) {
+	fmt.Println("recieve get request from:", r.RemoteAddr)
+	r.ParseForm()
+	if r.Method == http.MethodGet {  
+        // fmt.Println("key1", r.Form["key1"])   
+
+        // for k, v := range r.Form {  
+        //     fmt.Print("key:", k, "; ")  
+        //     fmt.Println("val:", strings.Join(v, ""))  
+        // }
+        if res, err := getListJsonStr(); err != nil{
+			
+			w.WriteHeader(http.StatusNotFound)//404
+	        return
+		}else{
+			fmt.Println("return to client:", res)
+			fmt.Fprintf(w, res)
+		}
+    }else{
+    	w.WriteHeader(http.StatusNotFound)//404
+    }
+}
+
+func handleAddThread(w http.ResponseWriter, r * http.Request) {
+	fmt.Println("recieve post request from:", r.RemoteAddr)
+	r.ParseForm()
+
+	if r.Method == http.MethodPost { 
+		result, _:= ioutil.ReadAll(r.Body)  
+        r.Body.Close()  
+        fmt.Println("post body:", string(result))
+  
+  
+        // var f interface{}  
+        // json.Unmarshal(result, &f)   
+        // m := f.(map[string]interface{})  
+        // for k, v := range m {  
+        //     switch vv := v.(type) {  
+        //     case string:  
+        //             fmt.Println(k, "is string", vv)  
+        //     case int:  
+        //             fmt.Println(k, "is int", vv)  
+        //     case float64:  
+        //             fmt.Println(k,"is float64",vv)  
+        //     case []interface{}:  
+        //             fmt.Println(k, "is an array:")  
+        //             for i, u := range vv {  
+        //                     fmt.Println(i, u)  
+        //             }  
+        //     default:  
+        //             fmt.Println(k, "is of a type I don't know how to handle")   
+        //     }  
+        //   }  
+  
+        newobj := Thread{}
+    	if err := json.Unmarshal(result, &newobj); err !=nil{
+			fmt.Println("net error Unmarshal:", string(result))
+			w.WriteHeader(http.StatusNotFound)//404
+	        return
+		}else{
+			fmt.Println("net success Unmarshal:", string(result))
+			if err2 := list.add(newobj); err2 != nil{
+				
+				w.WriteHeader(http.StatusNotFound)//404
+		        return
+			}
+		}
+
+		if res, err := getListJsonStr(); err != nil{
+			w.WriteHeader(http.StatusNotFound)//404
+	        return
+		}else{
+			fmt.Println("return to client:", res)
+			fmt.Fprintf(w, res)
+		}
+  
+		
+    }else{
+    	w.WriteHeader(http.StatusNotFound)//404
+    }
+	
+}
+
 func main() {
 	if _, err := os.Stat("data.txt"); os.IsNotExist(err) {
 	  if _, err2 := os.Create("data.txt"); err2 !=nil{
-	  	log.Fatal(err2)
+	  	
         return
 	  }
 	}
 	loadFromFile()
-	fmt.Println("stored lists are:"list)
+	fmt.Println("stored lists are:")
+	for _, line := range list.get(){
+		fmt.Println(line)
+	}
+	fmt.Println("----------------")
+	
+
+	
 
 	// t1 := Thread{
 	// 	Author:"lee",
@@ -96,12 +216,13 @@ func main() {
 
 	// saveToFile(t1)
 	// saveToFile(t2)
-
-	
 	
 
 	// ioutil.WriteFile("data.txt", []byte("aaa"), 0600)
 
+	http.HandleFunc("/MessageBoard/getList", handleGetList)
+	http.HandleFunc("/MessageBoard/addThread", handleAddThread)
+	http.ListenAndServe(":40404", nil)
 	
 }
 
